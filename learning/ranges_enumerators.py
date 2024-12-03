b5fe89cc1fa34fd22fca065620c5b9ec87e26364
Suarez <vssuarez1@cps.k12.il.us>
2024-12-03 09:34:35 -0600

    enumerators

--- learning/ranges_enumerators.py
+++ learning/ranges_enumerators.py
@@ -5,16 +5,22 @@
 # Range Practice #1
 # Create a list consisting of all the numbers from 2500 to 2585 (inclusive). Store this list in the variable my_list.
 
+my_list = list(range(2500, 2586))
+print(my_list)
 
 
 # Range Practice #2
 # Using the range() function, create in a single line of code a list consisting of all numbers that are multiples of 3 from 3 to 300 (inclusive). Store this list in the variable my_list.
 
+my_list = list(range(3, 301, 3))
+print(my_list)
 
 
 # Range Practice #3
 # Use the range() function and a loop to add the squares of all the numbers from 1 to 15 (inclusive). Store the result in a variable called sum_squares.
 
+sum_squares = sum([x**2 for x in range(1, 16)])
+print(sum_squares)
 
 
 # For this purpose:
@@ -28,6 +34,33 @@
 
 
 ##############################enumerators in python #####################################################
+
+my_list = ["apples", "pears", "oranges", "fruits"]
+
+for x, element in enumerate(my_list):
+    print(x, element)
+
+
+# Way 1
+# count = 0 
+# for element in my_list:
+#     print(element)
+#     if count == 1:
+#         print("count is 1")
+
+# count += 1
+
+# # Way 2
+# for x in range(len(my_list))
+
+
+
+
+
+
+
+
+
 
 
 # Enumerator Practice #1
