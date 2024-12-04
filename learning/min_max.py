572942a6afef55f983b423a4fb7cde084341724e
Suarez <vssuarez1@cps.k12.il.us>
2024-12-04 08:29:58 -0600

    random

--- learning/min_max.py
+++ learning/min_max.py
@@ -5,15 +5,17 @@
 # Min & Max Practice #1
 # Get the maximum value among the values in the following list, and store it in a variable named maximum_value:
 
-# number_list = [44542247/2, 21310/5, 2134747*33, 44556475, 121676, 6654067, 353254, 123134, 55**12, 611**5]
-
-
+number_list = [44542247/2, 21310/5, 2134747*33, 44556475, 121676, 6654067, 353254, 123134, 55**12, 611**5]
+maximum_value = max(number_list)
+print(maximum_value)
 
 
 # Min & Max Practice #2
 # Calculate the difference between the maximum and minimum value in the following list of numbers, and store it in a variable called number_range:
 
-# number_list = [44542247, 21310, 2134747, 44556475, 121676, 6654067, 353254, 123134, 552512, 611665]
+number_list = [44542247, 21310, 2134747, 44556475, 121676, 6654067, 353254, 123134, 552512, 611665] 
+number_range = max(number_list) - min(number_list) 
+print(number_range)
 
 
 # Min & Max Practice #3
--- learning/random_2.py
+++ learning/random_2.py
@@ -1,14 +1,31 @@
+import random
+
+x = random.randint(1,6)
+y = random.random()
+print(y)
+myList = ["rock", "paper", "scissors"]
+z = random.choice(myList)
+print(z)
+
+cards = [1, 2, 3,4,5,6,7,8,9,"J","Q","K","A"]
+random.shuffle(cards)
+print(cards)
+
+
 
 ################################################random in python#################################################
 # Random Practice #1
 # Implement the randint() function from the random library that allows you to obtain an integer from 1 to 10, and store that value in a variable called random_number.
-
+random_number = random.randint(1,10)
+print(random_number)
 
 # Random Practice #2
 # Implement the random() function from the random library to obtain a real number between 0 and 1, and store that value in a variable called random_number.
-
+random_number2 = random.random()
+print(random_number2)
 
 # Random Practice #3
 # Use the random library's choice() method to get a random item from the list of names below, and store the chosen name in a variable called raffle.
-
-# names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+raffle = random.choice(names)
+print(raffle)
